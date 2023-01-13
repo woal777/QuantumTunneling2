3331f83e4eeb7da66b56fb0ec5a76c551d83697d
woal777 <woal7949@gmail.com>
2023-01-13 17:02:33 +0900

    Update varying.py

--- tunneling/varying.py
+++ tunneling/varying.py
@@ -1,4 +1,4 @@
-from tunneling.main import Current, np, plt
+from main import Current, np, plt
 import time
 
 init = time.time()
@@ -8,13 +8,12 @@ x = np.append(x, np.logspace(-3, -0.5, 6))
 x = np.append(x, np.linspace(.5, 1.5, 12))
 
 barrier = [0, *np.linspace(3, 1, 20), 0]
-barrier[10] = 0
+# barrier[10] = 0
 c = Current(np.array(barrier))
 c.m = .05
-c.dx *= 3
+c.dx = 2e-10
+c.num_estep = 1000
 y = [c.current(r) for r in x]
-plt.semilogy(x, np.abs(y), label='right-dead')
-plt.legend()
+plt.semilogy(x, np.abs(y))
 print(time.time() - init)
-np.savetxt('down.dat', np.array((x, y)).transpose())
 plt.show()
